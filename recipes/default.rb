543afc2c442d6f4eabba5461a9362d3f2c4818a8
Mike Nicholaides <mike.nicholaides@gmail.com>
2013-08-27 13:05:19 -0400

    Only install bundler, and install it with the newly installed rubygems

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,12 +7,9 @@
 # All rights reserved - Do Not Redistribute
 #
 
-# http://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.3-p374.tar.gz
-
-
 packages = value_for_platform_family(
-             "rhel" => ["openssl-devel","zlib-devel","readline-devel", "libyaml-devel"],
-             "default" => ["libssl-dev","zlib1g-dev","libreadline-dev", "libyaml-dev"]
+             "rhel"    => %w[openssl-devel zlib-devel readline-devel  libyaml-devel],
+             "default" => %w[libssl-dev    zlib1g-dev libreadline-dev libyaml-dev]
            )
 
 packages.each do |dev_pkg|
@@ -20,12 +17,9 @@ packages.each do |dev_pkg|
 end
 
 remote_file "#{Chef::Config[:file_cache_path]}/ruby-#{node[:ruby][:version]}.tar.gz" do
-  # Include a not_if to prevent download on a bootstrap where the ruby version already
-  # matches or when running under chef-solo (which deletes the tar file). Checksum will
-  # handle things from there.
-  not_if "#{node[:ruby][:prefix]}/bin/ruby -v | grep \"#{node[:ruby][:version].gsub('-', '')}\""
   source "http://ftp.ruby-lang.org/pub/ruby/#{node[:ruby][:version][0..2]}/ruby-#{node[:ruby][:version]}.tar.gz"
-  checksum node[:ruby][:checksum]
+  checksum node[:ruby][:checksum] if  node[:ruby][:checksum]
+  not_if "#{node[:ruby][:prefix]}/bin/ruby -v | grep \"#{node[:ruby][:version].gsub('-', '')}\""
 end
 
 bash "install_ruby" do
@@ -33,29 +27,11 @@ bash "install_ruby" do
   cwd Chef::Config[:file_cache_path]
   code <<-EOH
     tar --no-same-owner -zxf ruby-#{node[:ruby][:version]}.tar.gz
-    (cd ruby-#{node[:ruby][:version]}/ && ./configure #{node[:ruby][:configure_opts]} && make #{node[:ruby][:make_opts]} && make install)
+    cd ruby-#{node[:ruby][:version]}/
+    ./configure #{node[:ruby][:configure_opts]}
+    make #{node[:ruby][:make_opts]}
+    make install
+    #{node[:ruby][:prefix]}/bin/gem install bundler
   EOH
-  action :nothing
-end
-
-bash "ensure_ruby_install" do
   not_if "#{node[:ruby][:prefix]}/bin/ruby -v | grep \"#{node[:ruby][:version].gsub('-', '')}\""
-  user "root"
-  notifies :run, "bash[install_ruby]", :immediately
-  notifies :install, "gem_package[bundler]", :immediately
-  notifies :install, "gem_package[foreman]", :immediately
-  notifies :install, "gem_package[chef]", :immediately
-end
-
-gem_package "bundler" do
-  options('--pre')
-  action :nothing
-end
-
-gem_package "foreman" do
-  action :nothing
-end
-
-gem_package "chef" do
-  action :nothing
 end
